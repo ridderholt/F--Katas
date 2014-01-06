10a10609c5b1c6897821322cf3248565d8deadc9
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-06 18:59:48 +0100

    Added a small kata for revesing words

---
+++ BackwardsWord/BackwardsWord/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let ReverseWord (word:string) = 
+    word.ToCharArray() |> Seq.toList |> List.rev |> List.map (fun c -> string c) |> String.concat ""
+
+
+[<EntryPoint>]
+let main argv = 
+
+    printf "%s\r\n" <| ReverseWord "Robin"
+    
+    printf "%s\r\n" <| ReverseWord "niboR"
+
+    0 // return an integer exit code
