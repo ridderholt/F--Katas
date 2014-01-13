f758253ffd8465401c55cfddab96e8d8fac9f352
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-13 18:48:42 +0100

    Extracted FizzBuzz logic into F# class

---
+++ FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzz
+type FizzBuzz() =
+    member this.Calculate (n:int) = 
+        match n with
+        | i when i%3 = 0 && i%5 = 0 -> "FizzBuzz"
+        | i when i%3 = 0 -> "Fizz"
+        | i when i%5 = 0 -> "Buzz"
+        | _ -> n.ToString()
+
+    member this.Calculate2 (n:int) =  
+        if n%3 = 0 && n%5 = 0 then
+            "FizzBuzz"
+        elif n%3 = 0 then
+            "Fizz"
+        elif n%5 = 0 then
+            "Buzz"
+        else
+            n.ToString()
--- FizzBuzz/Program.fs
+++ FizzBuzz/Program.fs
@@ -1,27 +1,9 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-let fizzBuzz n = 
-    match n with
-    | i when i%3 = 0 && i%5 = 0 -> "FizzBuzz"
-    | i when i%3 = 0 -> "Fizz"
-    | i when i%5 = 0 -> "Buzz"
-    | _ -> n.ToString()
-      
-
-let fizzBuzz2 n = 
-    if n%3 = 0 && n%5 = 0 then
-        "FizzBuzz"
-    elif n%3 = 0 then
-        "Fizz"
-    elif n%5 = 0 then
-        "Buzz"
-    else
-        n.ToString()
+﻿open FizzBuzz
 
 [<EntryPoint>]
 let main argv = 
     let arr = [1..20]
+    let fizz = FizzBuzz()
     
-    arr |> List.map fizzBuzz2 |> List.iter (fun n -> printf "%s\r\n" n)
+    arr |> List.map fizz.Calculate |> List.iter (fun n -> printf "%s\r\n" n)
     0 // return an integer exit code
