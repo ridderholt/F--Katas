1f056820d41a2712354d63c672b261e6a0ee4828
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-13 21:32:51 +0100

    Adds tests to BackwardsWord

---
+++ BackwardsWord/BackwardsWord.Test/Tests/BackwordTests.cs
@@ -0,0 +1,28 @@
+﻿using StringUtil;
+using Xunit;
+
+namespace BackwardsWord.Test.Tests
+{
+    public class BackwordTests
+    {
+        [Fact]
+        public void Should_Return_Green()
+        {
+            var backwords = new Backwords();
+            var expected = "Green";
+            var actual = backwords.Parse("neerG");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Return_neerG()
+        {
+            var backwords = new Backwords();
+            var expected = "neerG";
+            var actual = backwords.Parse("Green");
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ BackwardsWord/BackwardsWord/Backwords.fs
@@ -0,0 +1,5 @@
+﻿namespace StringUtil
+type Backwords() = 
+    member this.Parse (word:string) = 
+        word.ToCharArray() |> Seq.toList |> List.rev |> List.map (fun c -> string c) |> String.concat ""
+
