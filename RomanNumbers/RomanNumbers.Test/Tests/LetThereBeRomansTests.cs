a1c7240419121e57e246944051cf8ebbc970dec1
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-15 17:29:16 +0100

    Adds tests to RomanNumbers

---
+++ RomanNumbers/RomanNumbers.Test/Tests/LetThereBeRomansTests.cs
@@ -0,0 +1,18 @@
+﻿using Roman;
+using Xunit;
+
+namespace RomanNumbers.Test.Tests
+{
+    public class LetThereBeRomansTests
+    {
+        [Fact]
+        public void Shold_return_V()
+        {
+            var romans = new LetThereBeRomans();
+            var expected = "V";
+            var actual = romans.ToRoman(5);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
