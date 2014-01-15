eeb47f6324ab4149f1c68d29116d5c741db8969d
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-15 22:57:46 +0100

    Adds tests for a number of numbers

--- RomanNumbers/RomanNumbers.Test/Tests/LetThereBeRomansTests.cs
+++ RomanNumbers/RomanNumbers.Test/Tests/LetThereBeRomansTests.cs
@@ -6,12 +6,63 @@ namespace RomanNumbers.Test.Tests
 {
     public class LetThereBeRomansTests
     {
+        private readonly LetThereBeRomans _romans;
+
+        public LetThereBeRomansTests()
+        {
+            _romans = new LetThereBeRomans();
+        }
+
+        [Fact]
+        public void Should_return_X()
+        {
+            var expected = "X";
+            var actual = _romans.ToRoman(10);
+
+            Assert.Equal(expected, actual);
+        }
+
         [Fact]
         public void Shold_return_V()
         {
-            var romans = new LetThereBeRomans();
             var expected = "V";
-            var actual = romans.ToRoman(5);
+            var actual = _romans.ToRoman(5);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_return_L()
+        {
+            var expected = "L";
+            var actual = _romans.ToRoman(50);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_return_C()
+        {
+            var expected = "C";
+            var actual = _romans.ToRoman(100);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_return_M()
+        {
+            var expected = "M";
+            var actual = _romans.ToRoman(1000);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_return_MCCCXXXVII()
+        {
+            var expected = "MCCCXXXVII";
+            var actual = _romans.ToRoman(1337);
 
             Assert.Equal(expected, actual);
         }
