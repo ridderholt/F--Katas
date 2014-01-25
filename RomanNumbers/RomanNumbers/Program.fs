6bfb4a940f660ed247e8fbb6585161f5fa09d059
Robin Ridderholt <robin.ridderholt@live.com>
2014-01-25 22:35:46 +0100

    Optimized brute forze conversion from roman to decimal

--- RomanNumbers/RomanNumbers/Program.fs
+++ RomanNumbers/RomanNumbers/Program.fs
@@ -5,7 +5,6 @@ let roman = LetThereBeRomans()
 let rNum = "MCCCXXXVII"
 
 let filterRoman(n:int) = 
-    printf "%d\r\n" n
     roman.ToRoman(n) = rNum
 
 [<EntryPoint>]
@@ -16,6 +15,6 @@ let main argv =
 
     let force = [1..3000]
 
-    force |> List.filter (fun n -> filterRoman n ) |> List.head |> printf "%d\r\n"
+    force |> List.find (fun n -> filterRoman n ) |> printf "%d\r\n"
 
     0
