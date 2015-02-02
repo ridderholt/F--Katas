695c1dd130c5da398a3be34f9c4abdc430952946
Robin Ridderholt <robin.ridderholt@laget.se>
2015-02-02 21:41:46 +0100

    Refacotring

--- FizzBuzz/FizzBuzz.fs
+++ FizzBuzz/FizzBuzz.fs
@@ -1,11 +1,12 @@
 ﻿namespace FizzBuzz
+
 type FizzBuzz() =
     member this.Calculate n = 
         match (n%3,n%5) with
         | (0,0) -> "FizzBuzz"
         | (0,_) -> "Fizz"
         | (_,0) -> "Buzz"
-        | (_,_) -> string n
+        | _ -> string n
 
     member this.Calculate3 n =
         match n with
